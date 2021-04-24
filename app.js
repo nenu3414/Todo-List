b35f5c4f0e06bcf0f6201cec8fa3f6ace3d5057d
nenu3414 <55359009+nenu3414@users.noreply.github.com>
2021-04-24 23:16:48 +0530

    generating todos

---
+++ app.js
@@ -0,0 +1,34 @@
+//Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+//Event Listeners
+todoButton.addEventListener("click", addTodo);
+
+//Functions
+
+function addTodo(e) {
+  //Prevent refreshing the browser
+  e.preventDefault();
+  //Todo Div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  //Create Li
+  const newTodo = document.createElement("li");
+  newTodo.innerText = "hey";
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+  //Complete Button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+  //Trash Button
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+  //Append to list
+  todoList.appendChild(todoDiv);
+}
